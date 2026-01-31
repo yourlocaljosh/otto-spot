1ab694f5ce82f1031956e99e316daddb3bfad54c
Josh Hong <joshsw@umich.edu>
2026-01-31 14:14:20 -0500

    Add HSV color detection to grab barbell color

--- src/camera_test.py
+++ src/camera_test.py
@@ -1,21 +1,27 @@
 import cv2
+import numpy as np
+
+def pick_color(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        pixel_hsv = hsv[y, x]
+        print(f"Coords: ({x}, {y})")
+        print(f"HSV: {pixel_hsv}")
+        print(f"H: {pixel_hsv[0]}, S: {pixel_hsv[1]}, V: {pixel_hsv[2]}")
+        print(f"range:")
+        print(f"lower_red = np.array([{max(0, pixel_hsv[0]-10)}, {max(50, pixel_hsv[1]-50)}, {max(50, pixel_hsv[2]-50)}])")
+        print(f"upper_red = np.array([{min(180, pixel_hsv[0]+10)}, 255, 255])")
 
 cap = cv2.VideoCapture(0)
-
-if not cap.isOpened():
-    print("Error: Cannot open camera")
-    exit()
-
-print("Camera opened. Press 'q' to quit.")
+cv2.namedWindow('HSV get color')
+cv2.setMouseCallback('hsv getcolor', pick_color)
 
 while True:
     ret, frame = cap.read()
-    
     if not ret:
-        print("Error: Cannot read frame")
         break
     
-    cv2.imshow('Webcam', frame)
+    cv2.imshow('Color', frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
